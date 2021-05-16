453663c6dd59c3317be052d13684339d82a8594b
lyubomyr <lubomyr31@gmail.com>
2021-05-16 12:39:50 +0300

    api implementation

---
+++ src/api/api.js
@@ -0,0 +1,39 @@
+import {getUserFromStorage} from '../helpers/authHelpers';
+
+const API_HOST = 'https://jsonplaceholder.typicode.com';
+
+const getAuthUserId = async () => {
+  const user = await getUserFromStorage();
+  return user?.id;
+};
+
+export const getUsers = async params => {
+  const url = `${API_HOST}/users${params ? params : ''}`;
+  return await fetch(url).then(response => response.json());
+};
+
+export const getPosts = async () => {
+  const userId = await getAuthUserId();
+  const url = `${API_HOST}/posts?userId=${userId}`;
+  return await fetch(url).then(response => response.json());
+};
+
+export const getPhotos = async (albumId = 1) => {
+  const url = `${API_HOST}/photos?albumId=${albumId}`;
+  return await fetch(url).then(response => response.json());
+};
+
+export const getUserById = async id => {
+  const url = `${API_HOST}/users/${id}`;
+  return await fetch(url).then(response => response.json());
+};
+
+export const getPhotoById = async id => {
+  const url = `${API_HOST}/photos/${id}`;
+  return await fetch(url).then(response => response.json());
+};
+
+export const getComments = async id => {
+  const url = `${API_HOST}/posts/${id}/comments`;
+  return await fetch(url).then(response => response.json());
+};
