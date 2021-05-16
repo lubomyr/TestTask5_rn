6a08ef31dbdefaa074d60375857c02ae5e1d831f
lyubomyr <lubomyr31@gmail.com>
2021-05-16 12:43:23 +0300

    LoginScreen: impl. api call & logic

--- src/screens/login/LoginScreen.js
+++ src/screens/login/LoginScreen.js
@@ -1,13 +1,34 @@
 import React, {useState} from 'react';
 import {View} from 'react-native';
+import {Navigation} from 'react-native-navigation';
 import {globalStyles, theme} from '../../styles/globalStyles';
 import {FieldWrapper, TextInput, Button} from '../../components';
 import {getTopBarOptions} from '../../navigation/helper';
 import {withModal} from '../../hoc/withModal';
+import {getUsers} from '../../api/api';
+import {homeStack} from '../../navigation/navigationStacks';
+import {saveUserToStorage} from '../../helpers/authHelpers';
 
 const LoginScreen = props => {
   const {showModal} = props;
   const [data, setData] = useState({email: '', password: ''});
+
+  const checkEmail = async () => {
+    if (!data?.email) {
+      showModal({title: 'Enter valid email'});
+    }
+    if (!data?.password) {
+      showModal({title: 'Enter any password'});
+    }
+    const users = await getUsers(`?email=${data?.email}`);
+    if (users?.length) {
+      saveUserToStorage(users?.[0]);
+      Navigation.setStackRoot(props.componentId, homeStack);
+    } else {
+      showModal({title: 'User with this email not found'});
+    }
+  };
+
   return (
     <View style={globalStyles.root}>
       <FieldWrapper title={'Email'}>
@@ -27,11 +48,10 @@ const LoginScreen = props => {
         style={{marginTop: 30}}
         color={theme.primaryColor}
         title={'Login'}
-        onPress={() => {}}
+        onPress={() => checkEmail()}
       />
     </View>
   );
 };
-export default withModal(LoginScreen);
-
-LoginScreen.options = getTopBarOptions('Login');
+const options = getTopBarOptions('Login');
+export default withModal(LoginScreen, options);
